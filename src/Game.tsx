f7b56d025199feb5a8dddf7f64b69d40202f97e5
imamuray <imamuray1313@gmail.com>
2021-01-17 20:26:21 +0900

    盤面を表示できるようにした

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Game.css';
 
+function Square() {
+  const [isOn, setIsOn] = useState(false);
+  
+  return (
+    <button className="square"
+      onClick={() => setIsOn(!isOn)}>
+      {isOn ? "1" : "0"}
+    </button>
+  );
+}
+
+function Board() {
+  const size = 10;
+  const row = Array(size).fill(<td><Square /></td>);
+  const squares = Array(size).fill(<tr>{row}</tr>)
+  return (
+    <>
+      <table>{squares}</table>
+    </>
+  );
+}
 
 function Game() {
   return (
     <div className="game">
-      Hello
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <h1>Battleship Game</h1>
+        <p>
+          マスをクリックすると0と1が切り替わる
+        </p>
+      </div>
     </div>
   );
 }
